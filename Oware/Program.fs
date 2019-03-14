0b79578917d849954f39cef9070b78e0e4450d88
Sthabile Lushaba <G17L3794@ict.ru.ac.za>
2019-03-14 17:02:17 +0200

    Updated data structures

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,20 +1,52 @@
 ﻿module Oware
 
+
 type StartingPosition =
     | South
     | North
 
-let getSeeds n board = failwith "Not implemented"
+type Side =
+    | South
+    |North
 
+type House ={
+num : int
+side : Side
+seedsnum :int
+}
+
+type Gamestates =
+      |South's_Turn 
+      |North's_Turn
+      |Draw
+      |South_Won
+      |North_won
+
+type player ={
+houses : House*House*House*House*House*House
+ballsCollected : int
+}
+
+type Board = {
+player1 : player
+player2 : player
+state : Gamestates
+}
+
+let getSeeds n board = failwith "Not implemented"
+  
 let useHouse n board = failwith "Not implemented"
 
 let start position = failwith "Not implemented"
 
-let score board = failwith "Not implemented"
+let score board =
+    ()
+    failwith "Not implemented"
 
 let gameState board = failwith "Not implemented"
 
 [<EntryPoint>]
 let main _ =
     printfn "Hello from F#!"
+    //call the fun game
     0 // return an integer exit code
