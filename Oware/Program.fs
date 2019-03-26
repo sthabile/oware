ed753d8b97aa2dd825cff0e39e84dfec787bb597
stha <g17l3794@campus.ru.ac.za>
2019-03-26 23:56:50 +0200

    Did Functions Start and GameState

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -5,13 +5,9 @@ type StartingPosition =
     | South
     | North
 
-type Side =
-    | South
-    |North
 
 type House ={
-num : int
-side : Side
+HouseNum : int
 seedsnum :int
 }
 
@@ -37,13 +33,34 @@ let getSeeds n board = failwith "Not implemented"
   
 let useHouse n board = failwith "Not implemented"
 
-let start position = failwith "Not implemented"
+let start position =
+    let ReturnGame = {player1= {Houses=({{HouseNum=1};{seedsnum=0}},{{num=2};{seedsnum=0}},{{num=3};{seedsnum=0}}, {{num=4};{seedsnum=0}},{{num=5};{seedsnum=0}},{{num=6};{seedsnum=0}})} ; ballscollected =0};
+                     {player2= {Houses=({{HouseNum=7};{seedsnum=0}},{{num=8};{seedsnum=0}},{{num=9};{seedsnum=0}}, {{num=10};{seedsnum=0}},{{num=11};{seedsnum=0}},{{num=12};{seedsnum=0}})} ; ballscollected =0};
+                     state =  North's_Turn }
+ 
+                     (*an initiated game*)
+   
+   (*use a match expression to change the who is playing based on the input position*)
+    match position with
+    | South -> {ReturnGame with state = South's_Turn}
+    | North -> {ReturnGame with state = North's_Turn}
+    | _ -> failwith "Not implemented"
+    
 
 let score board =
     ()
     failwith "Not implemented"
 
-let gameState board = failwith "Not implemented"
+let gameState board =
+    match board with
+    |{player1=_;player2=_ ;state= Draw} -> "Draw"
+    |{player1=_;player2=_ ;state= North's_Turn} -> "Draw"
+    |{player1=_;player2=_ ;state= South's_Turn} -> "South's Turn"
+    |{player1=_;player2=_ ;state= North_won} -> "North Won"
+    |{player1=_;player2=_ ;state= South_Won} -> "South Won"
+    | _ ->  failwith "Not implemented"
+
+  
 
 [<EntryPoint>]
 let main _ =
